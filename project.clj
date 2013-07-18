95cfa1bb9dcb97c97ad71931c99d13d67f72eb4b
Roman Gonzalez <roman@birdseye-sw.com>
2013-07-18 13:52:21 -0700

    Update versions of clojure and lein-cljsbuild

--- project.clj
+++ project.clj
@@ -3,35 +3,32 @@
   :url "http://birdseye-sw.com/oss/buster-cljs/"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [potemkin "0.1.6"]]
-  :plugins [[lein-cljsbuild "0.2.9"]]
+  :plugins [[lein-cljsbuild "0.3.2"]]
   :source-paths ["src"]
   :cljsbuild
   {:builds
-   [{:id "dev"
-     :source-path "src"
+   [{:id "browser-test"
+     :source-paths ["src" "test"]
      :compiler
-     {:optimizations :simple
+     {:optimizations :whitespace
       :pretty-print true
+      :target :browser
       :externs ["resources/externs/buster.js"]
-      :output-to "resources/js/buster_cljs_dev.js"}}
-    ;;;
-    {:id "browser-test"
-     :source-path "test"
-     :notify-command ["./resources/buster_runner.sh"]
-     :compiler
-     {:optimizations :simple
-      :pretty-print true
-      :externs ["resouces/externs/buster.js"]
-      :libraries ["resources/js/buster_cljs_dev.js"]
       :output-to "resources/js/buster_cljs_browser_test.js"}}
-    ;;;
+    {:id "browser-test-optimized"
+     :source-paths ["src" "test"]
+     :compiler
+     {:optimizations :advanced
+      :pretty-print false
+      :target :browser
+      :externs ["resources/externs/buster.js"]
+      :output-to "resources/js/buster_cljs_browser_test.js"}}
     {:id "node-test"
-     :source-path "test"
-     :notify-command ["./resources/buster_runner.sh"]
+     :source-paths ["src" "test"]
      :compiler
      {:optimizations :simple
-      :libraries ["resources/js/buster_cljs.js"]
-      :externs ["resouces/externs/buster_dev.js"]
+      :target :node
+      :externs ["resources/externs/buster.js"]
       :output-to "resources/js/buster_cljs_node_test.js"}}]})
