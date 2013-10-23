bac6e4852fe25dcff0709e7f444e94da011fc900
Roman Gonzalez <roman@birdseye-sw.com>
2013-10-23 14:45:32 -0700

    New version of run-buster-tests.sh

--- run-buster-tests.sh
+++ run-buster-tests.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
-if pgrep -lf 'node.*buster-server' > /dev/null; then 
+
+[[ -e .buster-port ]] && BUSTER_PORT=$(cat .buster-port) || BUSTER_PORT=1111
+BUSTER_SERVER="http://localhost:${BUSTER_PORT}"
+BIN=./node_modules/.bin
+if pgrep -lf "node.*buster-server.*$BUSTER_PORT" > /dev/null; then 
     echo ">> buster-server is already running"
 else
     echo ">> Starting new buster-server"
-    buster-server &
+    $BIN/buster-server -p $BUSTER_PORT &
     BUSTER_PID=$!
     sleep 2
 fi
 
-if pgrep -lf 'phantomjs.*buster' > /dev/null; then 
+if pgrep -lf "phantomjs.*buster.*$BUSTER_PORT" > /dev/null; then 
     echo ">> A phantomjs slave is already running"
 else
     echo ">> Starting new phantomjs buster slave"
-    phantomjs ./node_modules/buster/script/phantom.js &
+    phantomjs ./node_modules/buster/script/phantom.js "$BUSTER_SERVER/capture" &
     PHANTOM_PID=$!
 fi
 
-buster-test -e node && buster-test -e browser
+$BIN/buster-test -s $BUSTER_SERVER
 RCODE=$?
 
 [[ -z $BUSTER_PID ]] || kill $BUSTER_PID
